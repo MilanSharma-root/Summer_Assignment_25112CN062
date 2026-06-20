5ab4373ca105bd2dfee26120d69d230c0d54c130
Milan Sharma <milan.root.sharma@gmail.com>
2026-06-20 21:49:08 +0530

    Added new code

---
+++ Day-20/Q79_FindRowWiseSum.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Q79_FindRowWiseSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows and columns: ");
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Row-wise Sum:");
+        for (int i = 0; i < rows; i++) {
+            int sum = 0;
+
+            for (int j = 0; j < cols; j++) {
+                sum += matrix[i][j];
+            }
+
+            System.out.println("Row " + (i + 1) + " = " + sum);
+        }
+    }
+}
